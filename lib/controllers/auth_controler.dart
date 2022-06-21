33764cdfdc2503ca8f31502116af00a7e7ac90c1
harsha.kuruwita <harsha.kuruwita@gmail.com>
2022-06-21 15:46:22 +0530

    pudh

--- lib/controllers/auth_controler.dart
+++ lib/controllers/auth_controler.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:web3dart/web3dart.dart';
 
@@ -8,6 +9,7 @@ class AuthControler extends GetxController {
   var isShowPassword = false.obs;
   var isShowConfirmPassword = false.obs;
   var ethAdddress = ''.obs;
+  late Web3Client ethClient;
 
   void setAcceptPrivacy({required bool isAccept}) {
     isAcceptPrivacy.value = isAccept;
@@ -26,5 +28,30 @@ class AuthControler extends GetxController {
     Credentials random = EthPrivateKey.createRandom(rnd);
     var address = await random.extractAddress();
     ethAdddress.value = address.toString();
+  }
+
+  Future<DeployedContract> loadContract() async {
+    String abi = await rootBundle.loadString('assets/abi/abi.json');
+    String contractAddress = ethAdddress.value;
+    final contract = DeployedContract(ContractAbi.fromJson(abi, 'Auth'),
+        EthereumAddress.fromHex(contractAddress));
+    return contract;
+  }
+
+  Future<List<dynamic>> query(String fuctionName, List<dynamic> arg) async {
+    final contract = await loadContract();
+    final ethFunction = contract.function(fuctionName);
+    final result = await ethClient.call(
+        contract: contract, function: ethFunction, params: arg);
+
+    return result;
+  }
+
+  Future<void> userRegister(
+      {required String walletAddress,
+      required String password,
+      required String email}) async {
+    List<dynamic> result =
+        await query('signup', [walletAddress, password, email]);
   }
 }
